b98fddee65d4800d30e221dc6acbc5ae1f9c7463
nikit <nikit97886@gmail.com>
2024-07-19 22:39:26 +0300

    client session trying

---
+++ session.py
@@ -0,0 +1,31 @@
+from config import TOKEN, API_ID, API_HASH
+import asyncio
+import logging
+from telethon import TelegramClient, events
+from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantsBots
+from telethon.errors import UserIsBlockedError, PeerIdInvalidError, UserPrivacyRestrictedError
+from run import links
+
+num_client = TelegramClient("tg", API_ID, API_HASH).start()
+
+
+async def get_members(links):
+    res = {}
+
+    for link in links:
+        try:
+            chat = await num_client.get_participants(link)
+
+            participants = []
+
+            async for user in chat:
+                username = user.username if user.username else f'{user.first_name} {user.last_name}'.strip()
+                participants.append(username)
+            res[link] = participants
+
+        except Exception as e:
+            res[link] = f'Произошла ошибка: {e}'
+
+    return res
+
+res = get_members(links)
