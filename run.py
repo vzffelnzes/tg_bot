aef615423dac7f3342b3fbecf12efa5f19c5c1bd
nikit <nikit97886@gmail.com>
2024-07-21 20:33:43 +0300

    pyrogram

--- run.py
+++ run.py
@@ -1,137 +1,34 @@
-from config import TOKEN, API_ID, API_HASH
-import asyncio
-import logging
-from telethon import TelegramClient, events, Button
-from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantsBots, ReplyInlineMarkup, \
-    ReplyKeyboardMarkup, PeerUser, KeyboardButtonRow, KeyboardButton
-from telethon.errors import UserIsBlockedError, PeerIdInvalidError, UserPrivacyRestrictedError
-from db.user import User
-from db.db_session import *
+from config import TOKEN
+from pyrogram import Client, filters, enums
+from pyrogram.errors import UserIsBlocked, PeerIdInvalid
+from test import get_user_list, send_message_to_users
+import logging, os, sys
 
-logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('telegram')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-global_init("db/base.db")
-
-# num_client = TelegramClient("tg", API_ID, API_HASH).start()
-client = TelegramClient("client1", API_ID, API_HASH).start()
+app = Client("my_bot", bot_token=TOKEN)
 
 
-@client.on(events.NewMessage(pattern='/start'))
-async def start(event):
-
-    await client.send_message(
-        await event.get_chat(),
-        "Привет! Это бот для парсинга аудитории в телеграмме, что бы ",
-
-    )
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply("Привет! Это бот для парсинга аудитории в телеграмме, что бы добавить пользователей в базу данных напиши /get_user_list <ссылки на чаты> <тематика>. Чтобы разослать сообщене напиши /send_message <тематика> <сообщение>")
 
 
-@client.on(events.NewMessage(pattern='Меню'))
-async def menu(event):
-    keyboard_buttons = [
-        [Button.inline("get_user_list", b"get_user_list"),
-         Button.inline("send_message_to_users", b"send_message_to_users")]
-    ]
-
-    await client.send_message(
-        event.chat_id,
-        "Выберите команду:",
-        buttons=keyboard_buttons
-    )
+@app.on_message(filters.command("get_user_list") & filters.regex(r"(.*)"))
+async def get_user_list(client, message):
+    await message.reply(get_user_list(message))
 
 
-@client.on(events.CallbackQuery)
-async def callback(event):
-    if event.data == b'get_user_list':
-        await event.respond("Напишите /get_user_list <ссылки> <категория>")
-    elif event.data == b'send_message_to_users':
-        await event.respond("Напишите /send_message_to_users <категория> <сообщения>")
+@app.on_message(filters.command("send_message") & filters.regex(r"(.*)"))
+async def send_message_to_users(client, message):
+    await message.reply(send_message_to_users(message))
 
 
-@client.on(events.NewMessage(pattern='/get_user_list (.*)'))
-async def get_user_list(event):
-    chat_links = event.pattern_match.group(1).split()[:-1]
-    category = event.pattern_match.group(1).split()[-1]
-    result = {}
-    # result = await get_members(chat_links, num_client)
 
-    for chat_link in chat_links:
-        try:
-            chat = await client.get_entity(chat_link)
-
-            admin_ids = set()
-            async for admin in client.iter_participants(chat, filter=ChannelParticipantsAdmins):
-                admin_ids.add(admin.id)
-
-            bot_ids = set()
-            async for bot in client.iter_participants(chat, filter=ChannelParticipantsBots):
-                bot_ids.add(bot.id)
-
-            participants = []
-            async for user in client.iter_participants(chat):
-                if user.id in admin_ids or user.id in bot_ids or user.is_self:
-                    continue
-
-                username = user.username if user.username else f'{user.first_name} {user.last_name}'.strip()
-                participants.append(username)
-
-                member = User()
-                member.user_id = str(user.id)
-                member.nickname = str(user.username)
-                member.category = str(category)
-                member.first_name = str(user.first_name)
-                member.last_name = str(user.last_name)
-                db_sess = create_session()
-                db_sess.add(member)
-                db_sess.commit()
-
-            result[chat_link] = participants
-
-        except Exception as e:
-            result[chat_link] = f'Произошла ошибка: {e}'
-
-    await event.reply(str(result))
-
-
-@client.on(events.NewMessage(pattern='/send_message (.*)'))
-async def send_message_to_users(event):
-    args = event.pattern_match.group(1).split(maxsplit=1)
-    if len(args) != 2:
-        await event.reply('Использование: /send_message <category> <message>')
-        return
-
-    db_session = create_session()
-
-    category, message = args
-    users = list(db_session.query(User).filter(User.category == category))
-    print(users)
-    sent_to = []
-    not_sent_to = []
-
-    try:
-        for user in users:
-            try:
-                await client.send_message(user.user_id, message)
-                sent_to.append(user.nickname if user.nickname else f'{user.first_name} {user.last_name}'.strip())
-            except (UserIsBlockedError, PeerIdInvalidError):
-                not_sent_to.append(user.nickname if user.nickname else f'{user.first_name} {user.last_name}'.strip())
-
-    except Exception as e:
-        await event.reply(f'Произошла ошибка: {e}')
-        return
-
-    response = f'Сообщение было отправлено:\n{sent_to}\n\nНе удалось отправить сообщение:\n{not_sent_to}'
-    await event.reply(response)
-
-
-def main():
-    client.run_until_disconnected()
-
-
-if __name__ == "__main__":
-    main()
+app.run()
--- test.py
+++ test.py
@@ -18,28 +18,30 @@ global_init("db/base.db")
 app = Client("mybot", api_id=API_ID, api_hash=API_HASH)
 
 
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply("Привет! Это бот для парсинга аудитории в телеграмме, что бы добавить пользователей в базу данных напиши /get_user_list <ссылки на чаты> <тематика>. Чтобы разослать сообщене напиши /send_message <тематика> <сообщение>")
-
-
-@app.on_message(filters.command("get_user_list") & filters.regex(r"(.*)"))
-async def get_user_list(client, message):
+async def get_user_list(message):
     chat_links = message.matches[0].group(1).split()[1:-1]
     category = message.matches[0].group(1).split()[-1]
     result = {}
 
+    print(1)
+    a = 0
     for chat_link in chat_links:
         aaa = chat_link.split("/")[-1]
+        print(1)
+        res = None
         try:
             chat = await app.get_chat(aaa)
             admin_ids = set()
             async for admin in chat.get_members(filter=enums.ChatMembersFilter.ADMINISTRATORS):
                 admin_ids.add(admin.user.id)
 
+            print(1)
+
             bot_ids = set()
-            async for bot in chat.get_members(filter=enums.ChatMembersFilter.ADMINISTRATORS):
+            async for bot in chat.get_members(filter=enums.ChatMembersFilter.BOTS):
                 bot_ids.add(bot.user.id)
+
+            print(1)
 
             participants = []
             async for user in chat.get_members():
@@ -60,44 +62,44 @@ async def get_user_list(client, message):
                 db_sess.commit()
 
             result[chat_link] = participants
-
+            print(1)
         except Exception as e:
+            a = 1
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
             print(e)
-            result[chat_link] = f"Произошла ошибка: {e}"
+            res = e
 
-    await message.reply(str(result))
+    if a == 0:
+        return "Успешно выполнено."
+    elif a == 1:
+        return f'Произошла ошибка: {res}'
 
 
-@app.on_message(filters.command("send_message") & filters.regex(r"(.*)"))
-async def send_message_to_users(client, message):
+async def send_message_to_users(message):
     args = message.matches[0].group(1).split()
     if len(args) != 3:
-        await message.reply("Использование: /send_message <category> <message>")
-        return
+        return "Использование: /send_message <category> <message>"
 
     db_session = create_session()
     category, message_text = args[1], args[2]
     users = list(db_session.query(User).filter(User.category == category))
-    sent_to = []
-    not_sent_to = []
+    sent_to = 0
+    not_sent_to = 0
 
     try:
         for user in users:
             try:
                 await app.send_message(user.user_id, message_text)
-                sent_to.append(user.nickname if user.nickname else f"{user.first_name} {user.last_name}".strip())
+                sent_to += 1 if user.nickname else f"{user.first_name} {user.last_name}".strip()
             except (UserIsBlocked, PeerIdInvalid):
-                not_sent_to.append(user.nickname if user.nickname else f"{user.first_name} {user.last_name}".strip())
+                not_sent_to += 1 if user.nickname else f"{user.first_name} {user.last_name}".strip()
 
     except Exception as e:
-        await message.reply(f"Произошла ошибка: {e}")
-        return
+        return (f"Произошла ошибка: {e}")
 
-    response = f"Сообщение было отправлено:\n{sent_to}\n\nНе удалось отправить сообщение:\n{not_sent_to}"
-    await message.reply(response)
+    return f"Сообщение было отправлено:\n{sent_to}\n\nНе удалось отправить сообщение:\n{not_sent_to}"
 
 
 app.run()
